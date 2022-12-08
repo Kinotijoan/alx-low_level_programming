5844426dd1f0068bbbcf66c4d9c7a8534a5a8c8d
Kinotijoan <kinotijoan@gmail.com>
2022-12-08 09:42:40 -0800

    prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description:prints a certain string
+ * puts(write a line or string to the output)
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}

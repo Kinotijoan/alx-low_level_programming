8c00e6d26c7f3e07e6ad5f4bb7a7aa9cacb03412
Kinotijoan <kinotijoan@gmail.com>
2022-12-08 09:52:22 -0800

    exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ * Description:prints a certain string
+ * printf(prints either a string or character)
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
+

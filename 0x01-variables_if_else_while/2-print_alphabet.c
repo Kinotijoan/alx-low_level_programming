0beef525eebe11b6baf82addf201a243b408d559
Kinotijoan <kinotijoan@gmail.com>
2022-12-09 09:02:55 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all the alphapets in lowercase
+ * use putchar
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	for (; ch < 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

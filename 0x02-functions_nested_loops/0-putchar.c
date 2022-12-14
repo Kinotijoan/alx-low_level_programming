eed588b58224611a8fba2950cd9e47daf63c4a5c
Kinotijoan <kinotijoan@gmail.com>
2022-12-14 08:52:54 -0800

    prints

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entrypoint
+ * Description: prints putchar and returns o
+ * printf - function that prints characters or strings
+ * Return: always (0) success
+ */
+
+int main(void)
+{
+
+	printf("_putchar");
+	return (0);
+}
